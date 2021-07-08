76d5c3a4ad5c60982d4ba46853d79f8b2ec48deb
akasoul <antonvoloshuk@gmail.com>
2021-07-08 12:28:07 +0300

    Initial Commit

---
+++ WAADSU_TestApp/WAADSU_TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAADSU_TestAppApp.swift
+//  WAADSU_TestApp
+//
+//  Created by Anton Voloshuk on 08.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct WAADSU_TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
